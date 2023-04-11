db2d2285023ddf41eca6f5f7a12638e28d0fb02d
sahinmelikov <1rima621melikov@gmail.com>
2023-04-12 03:41:08 +0400

    Acces Modifiers and Encaptulation

---
+++ Acces_Modifiers_Encaptulation/Person.cs
@@ -0,0 +1,124 @@
+﻿
+namespace Encaptulation
+{
+    internal class Person
+
+    {
+        /////properti methodu
+        //private string _name;
+        //public string Name
+        //{
+        //    get
+        //    {
+        //        return _name;
+        //    }
+        //    set
+        //    {
+        //        if (value.Length <= 2)
+        //        {
+        //            Console.WriteLine("Adivizi duzgun qeyd edin!");
+        //            return;
+        //        }
+        //        _name = value;
+        //        return;
+
+        //    }
+        //}
+        //private string _surname;
+        //public string Surname
+        //{
+        //    get { return _surname; }
+        //    set
+        //    {
+        //        _surname = value;
+        //    }
+        //}
+
+
+        //private int _age;
+        //public int Age
+        //{
+        //    set
+        //    {
+        //        if (age < 16)
+        //        {
+        //            Console.WriteLine("Yashiniz balacadir.");
+        //            return;
+        //        }
+
+        //    }
+        //    get
+        //    {
+        //        return _age;
+        //    }
+
+        //}
+        //private string _birthday;
+        //public string Birthday
+        //{
+        //    get
+        //    {
+        //        return _birthday;
+        //    }
+        //    set
+        //    {
+        //        _birthday = value;
+        //        return;
+        //    }
+        //}
+
+
+        public string name { get; set; }
+        public string surname { get; set; }
+        public int age { get; set; }
+        public string BirthDay { get; set; }
+
+
+
+
+        ///Encaptulation 
+        private string _name;
+        public void SetName(string value)
+        {
+            if (value.Length > 2)
+            {
+                _name = value;
+                return;
+            }
+            else
+            {
+                Console.WriteLine("adinizi duzgun daxil edin");
+            }
+        }
+        public string GetName()
+        {
+            return _name;
+        }
+
+
+        private int _age;
+        public void SetAge(int value)
+        {
+            if (value > 16)
+            {
+
+                _age = value;
+                return;
+              
+            }
+            else
+            {
+                Console.WriteLine("yasiniz catmir!");
+
+            }
+        }
+        public int GetAge()
+        {
+            return _age;
+        }
+
+
+    }
+
+
+}
---
+++ Acces_Modifiers_Encaptulation/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Encaptulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //////1)===>properti usulu
+
+            //Person person = new Person();
+            //Console.WriteLine("adinizi daxil edin");
+            //person.Name = Console.ReadLine();
+            //Console.WriteLine(person.name);
+            //Console.WriteLine("Soyadinizi daxil edin");
+            //person.Surname = Console.ReadLine();
+            //Console.WriteLine(person.Surname);
+            //Console.WriteLine("yasinizi daxil edin");
+            //person.age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(person.age);
+            //Console.WriteLine("dogum gunuvuzu qeyd edin");
+            //person.BirthDay = Console.ReadLine();
+            //Console.WriteLine(person.BirthDay);
+
+
+           ///// //Encaptulation 2)=====>
+            Person person = new Person();
+            person.SetName("s");
+            string name = person.GetName();
+            Console.WriteLine(name);
+            person.surname = "melikov";
+            Console.WriteLine(person.surname);
+            person.SetAge(2);
+            int age = person.GetAge();
+            Console.WriteLine(age);
+            person.BirthDay = "27 noyabr";
+            Console.WriteLine(person.BirthDay);
+        }
+    }
+}
